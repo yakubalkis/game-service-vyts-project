d7e7b8dd950f074195f5d6c5c1259083a54ccc0f
yakubalkis <alkis.yakub@gmail.com>
2023-12-11 01:17:21 +0300

    created user service details impl class for jwt.

---
+++ server/src/main/java/com/game/server/service/UserServiceDetailsImpl.java
@@ -0,0 +1,30 @@
+package com.game.server.service;
+
+import com.game.server.entity.User;
+import com.game.server.repository.UserRepository;
+import com.game.server.security.JwtUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceDetailsImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserServiceDetailsImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username);
+        return JwtUserDetails.create(user);
+    }
+
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).get();
+        return JwtUserDetails.create(user);
+    }
+}
