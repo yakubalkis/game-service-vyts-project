2ef36faf419e4652fe9959b3e9e0f6e67598c794
yakubalkis <alkis.yakub@gmail.com>
2023-12-11 01:19:33 +0300

    created to create user details object.

---
+++ server/src/main/java/com/game/server/security/JwtUserDetails.java
@@ -0,0 +1,87 @@
+package com.game.server.security;
+
+import com.game.server.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class JwtUserDetails implements UserDetails {
+
+    public Long id;
+    private String username;
+    private String password;
+    private String role;
+
+    public JwtUserDetails(Long id, String username, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public static JwtUserDetails create(User user) { // static method that gets User object and returns UserDetails object
+        return new JwtUserDetails(user.getId(), user.getUsername(), user.getPassword(), user.getRole().getRoleName());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
