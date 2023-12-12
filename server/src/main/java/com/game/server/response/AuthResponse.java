f411a690e5211b07b3a677380ee92e2d1098e2f5
yakubalkis <alkis.yakub@gmail.com>
2023-12-12 16:50:17 +0300

    created auth response object.

---
+++ server/src/main/java/com/game/server/response/AuthResponse.java
@@ -0,0 +1,30 @@
+package com.game.server.response;
+
+public class AuthResponse {
+    String message;
+    String username;
+
+    public AuthResponse() {
+    }
+
+    public AuthResponse(String message, String username) {
+        this.message = message;
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
