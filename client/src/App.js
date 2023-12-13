ba1a57f8a48bdf53c29b3b06c9953ccd77851a5f
yakubalkis <alkis.yakub@gmail.com>
2023-12-13 11:46:57 +0300

    added components to display.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
+import { Routes, Route } from 'react-router';
+import Header from "./components/Header";
+import Registration from './pages/auth/Registration';
+import Login from './pages/auth/Login';
+import UserHomePage from './pages/UserHomePage';
+import AdminHomePage from './pages/AdminHomePage';
 
 
 function App() {
   return (
-    <div className="App">
-      Hello Gamers
+    <div>
+      <div className="container">
+          <Header />
+
+          <Routes>
+            <Route path="/" element={<Registration />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/user/:username" element={<UserHomePage />} />
+            <Route path="/admin/:username" element={<AdminHomePage />} />
+          </Routes>
+
+      </div>
     </div>
   );
 }
